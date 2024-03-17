b69e5bf55cf13c97ae253d96d0e877d4a8f5d7d4
Konstantin_Sandanov <konstantinsandanov@yandex.ru>
2024-03-17 20:33:57 +0800

    Добавляем фильтер в наш проект

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'simpleapp',
     'django.contrib.sites',
     'django.contrib.flatpages',
     'fpages',
+    'django_filters'
 ]
 SITE_ID = 1
 
